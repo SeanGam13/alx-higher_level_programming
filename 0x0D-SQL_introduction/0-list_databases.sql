cb24770270a2a94cad29aa322c1923f9525c4d3c
Your Name <you@example.com>
2023-07-19 15:25:09 -0700

    Fav project

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,2 @@
+-- A script that lists all of the databases of the MySQL server.
+SHOW DATABASES;
---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- A scripts that computes the average score of all records in the table second_table.
+SELECT AVG(`score`) AS `average`
+FROM `second_table`;

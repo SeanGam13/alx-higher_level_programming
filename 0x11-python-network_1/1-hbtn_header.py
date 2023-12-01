1c7aa8153d83304460eaf7c17e5f55342ca864d3
SeanGam13 <seangambanou9@gmail.com>
2023-12-01 13:47:21 -0800

    theory test

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+xs#!/usr/bin/python3
 """This script takes in a URL, sends a request to the URL and displays the
 value of the X-Request-Id variable found in the header"""
 import sys
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,14 +2,13 @@
 """This script takes in a URL and an email, sends a POST request to the request to the
 passed URL with the email as a parameter, and displays the body of the response"""
 import sys
-import urllib.parse
-import urllib.request
+from urllib.request import urlopen
+from urllib.parse import urlencode
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
-
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+    email = sys.argv[2]
+    data = urlencode({'email': email}).encode('utf-8')
+    with urlopen(url, data) as response:
+        body = response.read()
+        print(body.decode('utf-8'))
